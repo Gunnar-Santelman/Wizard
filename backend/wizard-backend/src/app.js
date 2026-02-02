44b75f6b8a59baeff80c5cd3ef80b1551e4e2a55
Gunnar Santelman <gesantelman@gmail.com>
2026-02-02 11:59:43 -0600

    Set Up Hot Reload Capability With Docker

---
+++ backend/wizard-backend/src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express v2!");
+});
+
+export default app;
--- frontend/wizard-frontend/src/App.js
+++ frontend/wizard-frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hot Reload Test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
