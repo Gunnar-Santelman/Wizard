10fd3406f0d0a6c3cf56205882c8da5a3944c8bb
Gunnar Santelman <gesantelman@gmail.com>
2026-02-11 11:52:11 -0600

    Complete Create Database Connection

--- backend/wizard-backend/src/app.js
+++ backend/wizard-backend/src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
+import User from "./User.js";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express v2!");
+ app.get("/", (req, res) => {
+   res.send("Hello Express v2!");
 });
+
+// app.get("/test-user", async(req, res) => {
+//   const user = await User.create({
+//     username: "testuser",
+//     password: "FunTimesForevers"
+//   });
+//   res.json(user);
+// })
 
 export default app;
--- backend/wizard-backend/src/server.js
+++ backend/wizard-backend/src/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import connectDB from "./db.js";
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
