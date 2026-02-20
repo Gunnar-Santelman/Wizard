f439ad548d052abf8559d235559d8ae906a74d01
Augustine Campbell <gcampbell8@huskers.unl.edu>
2026-02-20 13:22:13 -0600

    Created Classes
    Added Card class+ functionality and a Suit enum simulation

---
+++ backend/wizard-backend/src/game/engine/Card.js
@@ -0,0 +1,24 @@
+class Card{
+    #suit;
+    #value;
+    #trump=false;
+    #sprite=null;
+    constructor(suit,val){
+        this.#suit=suit;
+        this.#value=val;
+    }
+   get suit(){return this.#suit};
+   get value(){return this.#value};
+   get trump(){return this.#trump};
+   get sprite(){return this.#sprite};
+   /**
+    * Boolean setter for trump.
+    * @param {*} state The desired state.
+    */
+   setTrump(state){
+    this.#trump=Boolean(state);
+   }
+   toString(){
+    return `${this.value} of ${this.suit}${this.trump?"(trump)":""}`
+   }
+}
---
+++ backend/wizard-backend/src/game/engine/Suit.js
@@ -0,0 +1,7 @@
+//Enum simulator as there is none in JS
+const SUIT=Object.freeze({
+    HEARTS:"hearts",
+    DIAMONDS:"diamonds",
+    CLUBS:"clubs",
+    SPADES:"spades"
+});
