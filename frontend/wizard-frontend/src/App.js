3487e032afc1bd1ede1aff63e8652a81a67b366b
Gunnar Santelman <gesantelman@gmail.com>
2026-02-25 16:18:42 -0600

    Add Firebase Capabilities

--- frontend/wizard-frontend/src/App.js
+++ frontend/wizard-frontend/src/App.js
@@ -6,8 +6,16 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import LobbyPage from "./LobbyPage";
 import GamePage from "./GamePage";
+//import {auth} from "./services/firebase.js";
 
 function App() {
+
+  // useEffect(() => {
+  //   console.log("API key loaded:", process.env.REACT_APP_FIREBASE_API_KEY);
+  //   console.log("Firebase Auth object:", auth);
+  //   console.log("Current user:", auth.currentUser)
+  // }, []);
+
   return (
     <Routes>
       <Route path = "/" element = {<Home />} />
