7970f1264fe0d3d45579ecbf9dc20fce15612267
CRun7789 <chantelle.runyan@gmail.com>
2026-02-25 17:24:05 -0600

    Implement Card.jsx

---
+++ frontend/wizard-frontend/src/components/Card.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+/*
+
+Possible Card States:
+1) DEFAULT   Default (valid and not hovered)
+
+2) VALID     Highlighted (valid and hovered, pops out of hand)
+    onMouseEnter and onMouseLeave tell you when it's being hovered
+
+3) INVALID   Grayscale (invalid)
+
+4) BACK      Back (other players' hands)
+
+*/
+
+export default function Card({ suit="spades", value=14, inPlayersHand=true }) {
+
+    const [isValidPlay, setIsValidPlay] = useState(false)
+    const [isHovered, setIsHovered] = useState(false)
+   
+    return (
+        <img
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+
+            // Shows either front or back of card, if it's in the player's hand or not
+            src={inPlayersHand ? "https://clipart-library.com/images/5TRrdzGjc.png" : "https://clipart-library.com/images/8cxrbGE6i.jpg"}
+
+            alt={value + " of " + suit}
+            className="card"
+            style={{
+                width: '120px',
+                height: '168px',
+                border: inPlayersHand && isHovered && isValidPlay ? "thick ridge lemonchiffon" : "thick ridge transparent",
+                borderRadius: '5px',
+                filter: inPlayersHand && isHovered && !isValidPlay ? "contrast(50%)" : "none",
+                transform: inPlayersHand && isHovered && isValidPlay ? "scale(1.1)" : "scale(1)",
+                transition: "transform 0.3s ease, border 0.3s ease, filter 0.3s ease",
+                cursor:inPlayersHand && isHovered && isValidPlay ? 'pointer' : 'not-allowed'
+             }}
+           
+        />
+    );
+}
+
