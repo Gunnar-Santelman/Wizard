b015f6cec081aa65fe2f59d7e018c1ccfc55af16
Gunnar Santelman <gesantelman@gmail.com>
2026-04-22 13:09:58 -0500

    Fix HomePage Conflict

--- frontend/wizard-frontend/src/pages/Home.js
+++ frontend/wizard-frontend/src/pages/Home.js
@@ -9,32 +9,42 @@ import "../styling/Home.css"
 import socket from "../socket";
 
 export default function Home() {
-    const { userData } = useAuth();
+  const { userData } = useAuth();
 
-    const [gameId, setGameId] = useState("");
-    const navigate = useNavigate();
-    const playerName = userData?.username;
+  const [gameId, setGameId] = useState("");
+  const navigate = useNavigate();
+  const playerName = userData?.username;
 
-    async function handleCreate() {
-        if (!playerName) return;
-        const game = await createGame();
+  async function handleCreate() {
+    if (!playerName) return;
+    const game = await createGame();
 
-        socket.emit("joinGame", {
-            gameId: game.id,
-            token: await getToken()
-        });
+    socket.emit("joinGame", {
+      gameId: game.id,
+      token: await getToken(),
+    });
 
-        navigate(`/lobby/${game.id}`, {
-            state: {playerName}
-        });
+    navigate(`/lobby/${game.id}`, {
+      state: { playerName },
+    });
+  }
+
+  async function handleJoin() {
+    if (!playerName) return;
+    socket.emit("joinGame", {
+      gameId,
+      token: await getToken(),
+    });
+  }
+
+  useEffect(() => {
+    function handleSuccess({ gameId }) {
+      navigate(`/lobby/${gameId}`, {
+        state: { playerName },
+      });
     }
-
-    async function handleJoin() {
-        if (!playerName) return;
-        socket.emit("joinGame", {
-            gameId,
-            token: await getToken()
-        });
+    function handleError(message) {
+      alert(message);
     }
 
     socket.on("joinSuccess", handleSuccess);
